60197755ee05de033eff96ce04fcdc139dd249f3
Jose <josecodetech@gmail.com>
2024-04-19 07:54:11 +0200

    primeros pasos

---
+++ 01_primeros_pasos.py
@@ -0,0 +1,14 @@
+import turtle  
+t = turtle.Turtle()
+t.forward(100)
+t.speed(1)
+t.backward(200)
+t.right(90)
+t.forward(150)
+t.left(45)
+t.forward(160)
+t.left(90)
+t.forward(200)
+for _ in range(4):
+    t.forward(100)
+    t.left(90)
